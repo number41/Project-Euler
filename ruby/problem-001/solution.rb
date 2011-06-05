a9e3e9b96b0d65d56e8440d7c54f119e2efb2565
Sean Peterson <William.S.Peterson@boeing.com>
2011-06-05 17:59:23 -0400

    Ruby 01: Efficient solution

--- ruby/problem-001/solution.rb
+++ ruby/problem-001/solution.rb
@@ -1,4 +1,10 @@
 #!/usr/local/bin/ruby
 
-numbers = (1..999).select {|x| x % 3 == 0 || x % 5 == 0}
-puts numbers.inject(:+)
+def sum_multiples(k, n)
+	upper = ((n-1)/k).floor
+	return k * ((upper * (upper+1))/2)
+end
+
+limit = 1000
+sum = sum_multiples(3, limit) + sum_multiples(5,limit) - sum_multiples(15,limit)
+puts sum
